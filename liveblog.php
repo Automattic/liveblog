5539d43898d22a63b12aac14755cb73e4e8d8ddb
Nikolay Bachiyski <nb@nikolay.bg>
2012-07-04 17:03:08 +0300

    Use self::$entries instead of self::get_entries*
    Consolidate all the querying through the
    WPCOM_Liveblog_Entries class.

--- liveblog.php
+++ liveblog.php
@@ -287,12 +287,11 @@ class WPCOM_Liveblog {
 	}
 
 	function add_liveblog_to_content( $content ) {
-		$post_id = get_the_ID();
-		$entries = self::get_entries_since( $post_id );
+		$entries = self::$entries->get( array( 'order' => 'ASC' ) );
 		$entries = array_reverse( $entries );
 
 		$liveblog_output = '';
-		$liveblog_output .= '<div id="liveblog-'. $post_id .'" class="liveblog-container">';
+		$liveblog_output .= '<div id="liveblog-'. self::$post_id .'" class="liveblog-container">';
 		$liveblog_output .= '<div class="liveblog-actions">';
 		$liveblog_output .= self::get_entry_editor_output();
 		$liveblog_output .= '</div>';
