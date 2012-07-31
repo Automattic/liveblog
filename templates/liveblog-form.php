0ee7df423367c930e1d9a6d66a9385d1e3ae7e1e
Nikolay Bachiyski <nb@nikolay.bg>
2012-07-31 21:47:38 +0300

    Mention images drag and drop in the text placeholder
    Otherwise writers wouldn't know that they can do that.
    
    Unfortunately no browser supports newlines in placeholders without
    extra javascript intervention. We will skip for now.

--- templates/liveblog-form.php
+++ templates/liveblog-form.php
@@ -1,6 +1,6 @@
 <fieldset id="liveblog-actions">
 	<legend><?php _e( "You're Live!", 'liveblog' ); ?></legend>
-	<textarea placeholder="<?php _e( 'Remember: keep it short!', 'liveblog' ); ?>" id="liveblog-form-entry" name="liveblog-form-entry" cols="50" rows="5"></textarea>
+	<textarea placeholder="<?php _e( "Remember: keep it short! To insert an image, drag and drop it here.", 'liveblog' ); ?>" id="liveblog-form-entry" name="liveblog-form-entry" cols="50" rows="5"></textarea>
 	<div class="liveblog-submit-wrapper">
 		<span id="liveblog-submit-spinner"></span>
 		<input type="button" id="liveblog-form-entry-submit" class="button" value="<?php echo esc_attr__( 'Publish Update', 'liveblog' ); ?>" />
