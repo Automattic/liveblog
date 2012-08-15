b4a4446d1fc272bf2527cdec9ecc4443f44d8aa1
Nikolay Bachiyski <nb@nikolay.bg>
2012-08-15 19:52:15 +0300

    Add a preview button

--- templates/liveblog-form.php
+++ templates/liveblog-form.php
@@ -3,6 +3,7 @@
 	<textarea placeholder="<?php esc_attr_e( "Remember: keep it short! To insert an image, drag and drop it here.", 'liveblog' ); ?>" id="liveblog-form-entry" name="liveblog-form-entry" cols="50" rows="5"></textarea>
 	<div class="liveblog-submit-wrapper">
 		<span id="liveblog-submit-spinner"></span>
+		<input type="button" id="liveblog-form-entry-preview" class="button" value="<?php esc_attr_e( 'Preview', 'liveblog' ); ?>" />
 		<input type="button" id="liveblog-form-entry-submit" class="button" value="<?php esc_attr_e( 'Publish Update', 'liveblog' ); ?>" />
 		<?php echo wp_nonce_field( self::nonce_key, self::nonce_key, false, false ); ?>
 	</div>
