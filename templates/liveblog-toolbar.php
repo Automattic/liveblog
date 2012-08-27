43e110fda08f70056cd149e9908e13531c13c2d2
Zach Tirrell <zbtirrell@gmail.com>
2012-08-27 12:04:36 -0400

    added toolbar UI component with play/pause link
    This adds the play/pause link which allows a reader
    to control the "nag" functionality.  This also includes
    a hook to allow other tools to be incorporated in the bar.

---
+++ templates/liveblog-toolbar.php
@@ -0,0 +1,5 @@
+<div id="liveblog-toolbar">
+	<a href="#pause_play" title="Pause Stream" class="pause-play">Pause Stream</a>
+	<div id="liveblog-update-spinner"></div>
+	<?php echo $more_tools; ?>
+</div>
