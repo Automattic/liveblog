46183dbbd4626de6a1cd7c78cc4d5130bd7cca1f
Nikolay Bachiyski <nb@nikolay.bg>
2012-12-17 18:11:51 +0200

    Update bootstrap.php path in sample bootstrap file
    The path just changed in core tests framework.

--- t/bootstrap-sample.php
+++ t/bootstrap-sample.php
@@ -5,4 +5,4 @@ $GLOBALS['wp_tests_options'] = array(
     'active_plugins' => array( 'liveblog/liveblog.php' ),
 );
 
-require rtrim( WP_TESTS_PATH, DIRECTORY_SEPARATOR ) . DIRECTORY_SEPARATOR . 'bootstrap.php';
+require rtrim( WP_TESTS_PATH, DIRECTORY_SEPARATOR ) . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'bootstrap.php';
