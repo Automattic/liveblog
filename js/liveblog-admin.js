281438ddfb6dc36c87b7198653d9478a9159b518
Nikolay Bachiyski <nb@nikolay.bg>
2012-12-20 02:00:18 +0200

    Use actual information for AJAX action
    Instead of post ID 1, use the one from the post page.
    
    Pass the nonce from the server-side and use that.

--- js/liveblog-admin.js
+++ js/liveblog-admin.js
@@ -1,11 +1,12 @@
 jQuery(function($) {
 	var $meta_box = $('#liveblog');
+	var post_id = $('#post_ID').val();
 	var show_error = function(message) {
 		$('p.error', $meta_box).show().html(message);
  	}
 	$meta_box.on('click', 'button', function(e) {
 		e.preventDefault();
-		var url = ajaxurl + '?action=set_liveblog_state_for_post&post_id=1&state=' + $(this).val();
+		var url = ajaxurl + '?action=set_liveblog_state_for_post&post_id=' + encodeURIComponent(post_id) + '&state=' + encodeURIComponent($(this).val()) + '&' + liveblog_admin_settings.nonce_key + '=' + liveblog_admin_settings.nonce;
 		$('.inside', $meta_box).load(url, function(response, status, xhr) {
 			if ( status != 'error') return;
 			show_error('Error: ' + xhr.status + ' ' + xhr.statusText);
