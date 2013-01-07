5e817b2fd899e39ffb5b1be01cadb3189c512a0b
Nikolay Bachiyski <nb@nikolay.bg>
2013-01-07 23:22:25 +0200

    Don't try to access non-existing property
    In older versions upload.attributes didn't exist,
    so in this case get file from the main upload object.

--- js/plupload.js
+++ js/plupload.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function($) {
 			var filename, percent;
 
 			if ( 'undefined' === typeof( file ) )
-				file = upload.attributes.file;
+				file = upload.attributes? upload.attribues.file : upload.file;
 
 			if( 'undefined' === typeof( file ) )
 				return;
