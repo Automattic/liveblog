95468ae6232dc4dcf0f9abd19ade18e70fb4174a
Nikolay Bachiyski <nb@nikolay.bg>
2013-02-28 11:18:43 +0200

    Travis the liveblog tests
    * Move the phpunit config to the root dir
    * Make bootstra.php depend on the env variable
    and deprecate the bootstrap-sample.php
    * Many thanks to @scribu and his wp-cli plugin tests
    scaffold!

--- t/bootstrap-sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-define( 'WP_TESTS_PATH', 'Enter the path to your unit-tests checkout' );
-
-$GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array( 'liveblog/liveblog.php' ),
-);
-
-require rtrim( WP_TESTS_PATH, DIRECTORY_SEPARATOR ) . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'bootstrap.php';
