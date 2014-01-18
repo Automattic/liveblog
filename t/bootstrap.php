9e6cfc750dff3be014492ee675f34aea3456a4b6
Nikolay Bachiyski <nb@nikolay.bg>
2014-01-17 18:22:33 -1000

    Update Travis infrastructure to latest wp-cli

--- t/bootstrap.php
+++ t/bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 
-$GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array( basename( dirname( dirname( __FILE__ ) ) ) . '/liveblog.php' ),
-);
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
-require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+require_once $_tests_dir . '/includes/functions.php';
+
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../liveblog.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require $_tests_dir . '/includes/bootstrap.php';
 
