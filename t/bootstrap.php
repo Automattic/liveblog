ad9bc3559e38f8fabbce00803aac959a44bdb2b7
Nikolay Bachiyski <nb@nikolay.bg>
2014-01-17 18:24:51 -1000

    Globalize $wp_query before the plugin is loaded
    $wp_query isn't properly globalized in wp-settings.php and when WP is
    loaded in a function (unit tests case), the global is missing.
    
    See https://core.trac.wordpress.org/ticket/26867 for a permanent fix.

--- t/bootstrap.php
+++ t/bootstrap.php
@@ -8,7 +8,16 @@ require_once $_tests_dir . '/includes/functions.php';
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../liveblog.php';
 }
+
+function _set_global_wp_query() {
+	if ( !isset( $GLOBALS['wp_query'] ) ) {
+		$GLOBALS['wp_the_query'] = new WP_Query;
+		$GLOBALS['wp_query'] = $GLOBALS['wp_the_query'];
+	}
+}
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+tests_add_filter( 'muplugins_loaded', '_set_global_wp_query' );
 
 require $_tests_dir . '/includes/bootstrap.php';
 
