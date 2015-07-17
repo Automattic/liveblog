a22649cdb2e42cc1e3fa52cd376440ba5af8a471
Jason Agnew <jason@bigbitecreative.com>
2015-07-17 16:44:13 +0100

    Updated timeline to use content format.

--- templates/liveblog-key-single-timeline.php
+++ templates/liveblog-key-single-timeline.php
@@ -1,6 +1,6 @@
 <li <?php echo $css_classes; ?> data-timestamp="<?php echo $timestamp; ?>">
 	<a href="#liveblog-entry-<?php echo $entry_id; ?>">
 		<span><?php echo $entry_date; ?><?php echo $entry_time; ?></span>
-		<?php echo $content; ?>
+		<?php echo WPCOM_Liveblog_Entry_Key_Events::get_formatted_content($content, $post_id); ?>
 	</a>
 </li>
