0b54d878b815b3ff5507b8258ee12da42035fa92
Paul Kevan <paul.kevan@metro.co.uk>
2015-08-21 16:34:54 +0100

    Changing preg to not break on mulitple words separated by hypens

--- classes/class-wpcom-liveblog-entry-extend-feature-hashtags.php
+++ classes/class-wpcom-liveblog-entry-extend-feature-hashtags.php
@@ -166,7 +166,7 @@ class WPCOM_Liveblog_Entry_Extend_Feature_Hashtags extends WPCOM_Liveblog_Entry_
 		if ( WPCOM_Liveblog::key == $comment->comment_type ) {
 
 			// Grab all the prefixed classes applied.
-			preg_match_all( '/(?<!\w)'.preg_quote( $this->class_prefix ).'\w+/', $comment->comment_content, $terms );
+			preg_match_all( '/(?<!\w)'.preg_quote( $this->class_prefix ).'(\w\-?)+', $comment->comment_content, $terms );
 
 			// Append the first class to the classes array.
 			$classes = array_merge( $classes, $terms[0] );
