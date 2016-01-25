60c5fb0dabccb5f81553518d71aa0eff2e950973
Rodrigo Primo <rodrigo@hacklab.com.br>
2016-01-25 16:43:00 -0200

    Let users configure Socket.io server port, Redis host and Redis port in the Socket.io server via command-line arguments
    This commit adds a Node module called "commander" to the Node app used to start the Socket.io server to let users configure Socket.io server port, Redis host and Redis port using the following syntax:
    
    node app.js --socketio-port [port] --redis-host [host] --redis-port [port]

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,7 +1,15 @@
-var redis = require('socket.io-redis');
-var io = require('socket.io')(3000);
+var program = require('commander');
 
-io.adapter(redis());
+program
+  .option('--socketio-port [port]', 'Set Socket.io server port (default: 3000)', 3000)
+  .option('--redis-host [host]', 'Set Redis server host (default: localhost)', 'localhost')
+  .option('--redis-port [port]', 'Set Redis server port (default: 6379)', 6379)
+  .parse(process.argv);
+
+var redis = require('socket.io-redis');
+var io = require('socket.io')(program.socketioPort);
+
+io.adapter(redis({ host: program.redisHost, port: program.redisPort }));
 
 // Handle opened socket connections
 io.on('connection', function(socket) {});
