0744d2414450070810e6ad6f653acd534c0a6823
Travis <tpfraz@gmail.com>
2016-02-20 11:53:06 -0800

    Minor cleanup of some of the code structure

--- classes/class-wpcom-liveblog-rest-api.php
+++ classes/class-wpcom-liveblog-rest-api.php
@@ -9,15 +9,31 @@
 
 class WPCOM_Liveblog_Rest_Api {
 
+	// TODO: Following REST conventions with method names such as register_routes, get_items, get_item, create_item, update_item and delete_item
+	// -- This might be helpful if further integration is needed when the full REST API is available in core
+ 	// -- See: http://v2.wp-api.org/extending/adding/
+	// TODO: use permission_callback for any restricted endpoints
+
 	/**
 	 * Register all of our endpoints
 	 */
 	public static function load() {
 
-		add_action( 'rest_api_init', function () {
-		    register_rest_route( 'liveblog/v1', '/entries_between/(?P<post_id>\d+)/(?P<start_time>\d+)/(?P<end_time>\d+)', array(
-		        'methods' => 'GET',
-		        'callback' => 'WPCOM_Liveblog::rest_api_entries_between',
+		add_action( 'rest_api_init', array( __CLASS__, 'register_routes' ) );
+		// add_action( 'admin_notices', array( 'WPCOM_Liveblog', 'show_old_wp_notice' ) );
+
+	}
+
+	public static function register_routes() {
+
+		$version = '1';
+        $namespace = 'liveblog/v' . $version;
+        $base = '';
+
+		register_rest_route( $namespace, '/' . $base . '/entries_between/(?P<post_id>\d+)/(?P<start_time>\d+)/(?P<end_time>\d+)',
+			array(
+		        'methods' => WP_REST_Server::READABLE,
+		        'callback' => array( 'WPCOM_Liveblog', 'rest_api_entries_between' ),
 		        'args' => array(
 		        	'post_id' => array(
 		            	'required' => true,
@@ -38,9 +54,8 @@ class WPCOM_Liveblog_Rest_Api {
 		                },
 		            ),
 		        ),
-		    ) );
-		} );
-
+	    	)
+	    );
 	}
 
 }
