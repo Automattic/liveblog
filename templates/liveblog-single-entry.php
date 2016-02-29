9368bda5e3bb3b639c05fcc399b5a5e28e91c694
Budzanowski <bbudz@wro-bbudz-ml-01.local>
2016-02-29 18:54:58 +0100

    Add additional tags to allow embedded video content in liveblog post

--- templates/liveblog-single-entry.php
+++ templates/liveblog-single-entry.php
@@ -5,7 +5,7 @@
 		<span class="liveblog-meta-time"><a href="#liveblog-entry-<?php echo absint( $entry_id ); ?>"><span class="date"><?php echo esc_html( $entry_date ); ?></span><span class="time"><?php echo esc_html( $entry_time ); ?></span></a></span>
 	</header>
 	<div class="liveblog-entry-text" data-original-content="<?php echo esc_attr( $original_content ); ?>">
-		<?php echo wp_kses_post( $content ); ?>
+		<?php echo wp_kses( $content, $allowed_tags_for_entry ); ?>
 	</div>
 <?php if ( $is_liveblog_editable ): ?>
 	<ul class="liveblog-entry-actions">
