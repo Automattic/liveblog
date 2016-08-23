Automattic/liveblog

commit a25f2d03d9de457adb379a277265e952e3136fab

Support custom templates by theme user in key events.

templates/liveblog-single-entry.php
@@ -2,7 +2,7 @@
 	<header class="liveblog-meta">
 		<span class="liveblog-author-avatar"><?php echo $avatar_img; ?></span>
 		<span class="liveblog-author-name"><?php echo $author_link; ?></span>
-		<span class="liveblog-meta-time"><a href="#liveblog-entry-<?php echo $entry_id; ?>"><span class="date"><?php echo $entry_date; ?></span><span class="time"><?php echo $entry_time; ?></span></a></span>
+		<span class="liveblog-meta-time"><a class="liveblog-time-update" href="#liveblog-entry-<?php echo $entry_id; ?>"><span class="date"><?php echo $entry_date; ?></span><span class="time"><?php echo $entry_time; ?></span></a></span>
 	</header>
 	<div class="liveblog-entry-text" data-original-content="<?php echo esc_attr( $original_content ); ?>">
 		<?php echo $content; ?>

classes/class-wpcom-liveblog-entry-key-events.php
@@ -21,10 +21,9 @@ class WPCOM_Liveblog_Entry_Key_Events {
 	/**
 	 * Template to render entries
 	 */
-	protected static $template;
 	protected static $available_templates = array(
-		'list'     => 'liveblog-key-single-list.php',
-		'timeline' => 'liveblog-key-single-timeline.php',
+		'list'     => array( 'liveblog-key-single-list.php', 'ul', 'liveblog-key-list' ),
+		'timeline' => array( 'liveblog-key-single-timeline.php', 'ul', 'liveblog-key-timeline' ),
 	);
 	protected static $available_formats = array(
 		'first-sentence'  => array( __CLASS__, 'format_content_first_sentence' ),
@@ -37,16 +36,22 @@ class WPCOM_Liveblog_Entry_Key_Events {
 	 * new command and shortcode.
 	 */
 	public static function load() {
-
-		self::$available_templates = apply_filters( 'liveblog_key_templates', self::$available_templates );
-		self::$available_formats   = apply_filters( 'liveblog_key_formats', self::$available_formats );
-
+		add_action( 'init',                           array( __CLASS__, 'add_templates' ), 11 );
 		add_filter( 'liveblog_active_commands',       array( __CLASS__, 'add_key_command' ), 10 );
 		add_filter( 'liveblog_entry_for_json',        array( __CLASS__, 'render_key_template' ), 10, 2 );
 		add_filter( 'liveblog_admin_add_settings',    array( __CLASS__, 'add_admin_options' ), 10, 2 );
 		add_shortcode( 'liveblog_key_events',         array( __CLASS__, 'shortcode' ) );
 		add_action( 'liveblog_command_key_after',     array( __CLASS__, 'add_key_action' ), 10, 3 );
 		add_action( 'liveblog_admin_settings_update', array( __CLASS__, 'save_template_option' ), 10, 3 );
+	}
+
+	/**
+	 * Add templates for the key events on init
+	 * so theme templates can add their own
+     */
+	public static function add_templates() {
+		self::$available_templates = apply_filters( 'liveblog_key_templates', self::$available_templates );
+		self::$available_formats   = apply_filters( 'liveblog_key_formats', self::$available_formats );
 	}
 
 	/**
@@ -83,7 +88,8 @@ class WPCOM_Liveblog_Entry_Key_Events {
      */
 	public static function render_key_template( $entry, $object ) {
 		$post_id      = $object->get_post_id();
-		$entry['key'] = $object->render( self::get_current_template( $post_id ) );
+		$template     = self::get_current_template( $post_id );
+		$entry['key'] = $object->render( $template[0] );
 		return $entry;
 	}
 
@@ -182,6 +188,7 @@ class WPCOM_Liveblog_Entry_Key_Events {
      */
 	public static function format_content_first_sentence( $content ) {
 		$content = preg_replace('/(.*?[?!.](?=\s|$)).*/', '\\1', $content);
+		$content = strip_tags( $content , '<strong></strong><em></em><span></span><img>' );
 		return $content;
 	}
 
@@ -193,8 +200,10 @@ class WPCOM_Liveblog_Entry_Key_Events {
 	 * @return string
      */
 	public static function format_content_first_linebreak( $content ) {
-		$content = explode('</p>', $content);
-		return $content[0];
+		$content = str_replace( array( "\r", "\n" ), '<br />', $content);
+		$content = explode('<br />', $content);
+		$content = strip_tags( $content[0] , '<strong></strong><em></em><span></span><img>' );
+		return $content;
 	}
 
 	/**
@@ -220,12 +229,15 @@ class WPCOM_Liveblog_Entry_Key_Events {
 		);
 		$entry_query = new WPCOM_Liveblog_Entry_Query( $post->ID, WPCOM_Liveblog::key );
 		$entries     = (array) $entry_query->get_all( $args );
+		$template    = self::get_current_template( $post->ID );
 
 		if ( WPCOM_Liveblog::get_liveblog_state( $post->ID ) ) {
 			return WPCOM_Liveblog::get_template_part( 'liveblog-key-events.php', array(
 				'entries'  => $entries,
 				'title'    => $atts['title'],
-				'template' => self::get_current_template( $post->ID ),
+				'template' => $template[0],
+				'wrap'     => $template[1],
+				'class'    => $template[2],
 			) );
 		}
 	}

css/liveblog-admin.css
@@ -11,3 +11,11 @@ div#liveblog p.error {
 	padding: 4px;
 	border-radius: 3px;
 }
+
+div#liveblog p.success {
+	display: none;
+	background-color: #93DB70;
+	border: 1px solid #4CBB17;
+	padding: 4px;
+	border-radius: 3px;
+}

css/liveblog.css
@@ -529,15 +529,16 @@ a.liveblog-form-entry::-webkit-input-placeholder {
 
 /* =Key Events
 -------------------------------------------------------------- */
-.liveblog-key-events .liveblog-entry-actions,
-.liveblog-key-events .liveblog-meta,
-.liveblog-key-events img {
-	display:none;
+
+.liveblog-key-events h2 {
+	color: #333;
+	margin-bottom: 15px;
 }
 
 .liveblog-key-events .liveblog-entry {
 	border: none;
-	cursor: pointer;
+	overflow: visible;
+	padding: 0px;
 }
 
 .liveblog-key-events .liveblog-entry a:hover {
@@ -546,6 +547,50 @@ a.liveblog-form-entry::-webkit-input-placeholder {
 
 .liveblog-key-events li.liveblog-entry p {
 	margin-bottom: 0px;
+}
+
+.liveblog-key-events .liveblog-key-timeline li {
+	display: block;
+	list-style: none;
+}
+
+.liveblog-key-events .liveblog-key-timeline li .link {
+	position: relative;
+	display: block;
+	padding: 0 10px 20px;
+	border-left: 1px solid #dfdfdf;
+}
+
+.liveblog-key-events .liveblog-key-timeline li .link:before {
+	display: block;
+	position: absolute;
+	content: ' ';
+	top: 2px;
+	left: -5px;
+	width: 10px;
+	height: 10px;
+	background-color: #bbb;
+ }
+
+.liveblog-key-events .liveblog-key-timeline li:first-child .link:before,
+.liveblog-key-events .liveblog-key-timeline li:last-child .link:before {
+	border-radius: 0;
+	top: 0;
+	height: 1px;
+}
+
+.liveblog-key-events .liveblog-key-timeline li:last-child .link {
+	border-color: transparent;
+}
+
+.liveblog-key-events .liveblog-key-timeline li .title {
+	display: inline;
+}
+
+.liveblog-key-events .liveblog-key-timeline li .date {
+	display: block;
+	font-size: 10px;
+	color: #333;
 }
 
 /* =Notification settings

js/liveblog-admin.js
@@ -13,6 +13,7 @@ jQuery( function( $ ) {
 		url += '&' + $('input, textarea, select', $meta_box).serialize();
 		$( '.inside', $meta_box ).load( url, function( response, status, xhr ) {
 			if ( status === 'success') {
+				$( 'p.success', $meta_box ).show(0).delay( 1000 ).hide(0);
 				return;
 			}
 			if (xhr.status && xhr.status > 200) {

js/liveblog.js
@@ -18,11 +18,11 @@ window.liveblog = window.liveblog || {};
 		},
 		updateTimes: function() {
 			var self = this;
-			this.$('.liveblog-entry').each(function() {
+			$('.liveblog-entry').each(function() {
 				var $entry = $(this),
 					timestamp = $entry.data('timestamp'),
 					human = self.formatTimestamp(timestamp);
-				$('.liveblog-meta-time a', $entry).text(human);
+				$('.liveblog-time-update', $entry).text(human);
 			});
 		},
 		formatTimestamp: function(timestamp) {

liveblog.php
@@ -743,7 +743,10 @@ final class WPCOM_Liveblog {
 	public static function get_template_part( $template_name, $template_variables = array() ) {
 		ob_start();
 		extract( $template_variables );
-		if( self::$custom_template_path && file_exists( self::$custom_template_path . '/' . $template_name ) ) {
+		if ( 0 === strpos( $template_name, '{theme}' ) ) {
+			$template_name = str_replace('{theme}', get_template_directory(), $template_name);
+			include( $template_name );
+		} else if( self::$custom_template_path && file_exists( self::$custom_template_path . '/' . $template_name ) ) {
 			include( self::$custom_template_path . '/' . $template_name );
 		} else {
 			include( dirname( __FILE__ ) . '/templates/' . $template_name );
@@ -792,9 +795,10 @@ final class WPCOM_Liveblog {
 			$active_text = __( 'This is a normal WordPress post, without a liveblog.', 'liveblog' );
 			$buttons['archive']['disabled'] = true;
 		}
+		$update_text  = __( 'Settings have been successfully updated.', 'liveblog' );
 		$extra_fields = array();
 		$extra_fields = apply_filters( 'liveblog_admin_add_settings', $extra_fields, $post->ID );
-		echo self::get_template_part( 'meta-box.php', compact( 'active_text', 'buttons', 'extra_fields' ) );
+		echo self::get_template_part( 'meta-box.php', compact( 'active_text', 'buttons', 'update_text', 'extra_fields' ) );
 	}
 
 	public static function admin_ajax_set_liveblog_state_for_post() {

templates/liveblog-key-events.php
@@ -1,10 +1,8 @@
 <div class="liveblog-key-events">
 	<h2><?php echo $title ?></h2>
-	<div id="liveblog-key-entries">
-		<?php foreach ( (array) $entries as $entry ) : ?>
-
+	<<?php echo $wrap ?> id="liveblog-key-entries" class="<?php echo $class ?>">
+	<?php foreach ( (array) $entries as $entry ) : ?>
 			<?php echo $entry->render( $template ); ?>
-
 	<?php endforeach; ?>
-	</div>
+	</<?php echo $wrap ?>>
 </div>

commit 94413aca370463ba2fbd550a449db71c5dc8238d

Restore the relative date feature in the liveblog.
The changes in https://github.com/Automattic/liveblog/commit/a25f2d03d9de457adb379a277265e952e3136fab broke the relative date feature in standard liveblog as the template used in the liveblog does not contain the `liveblog-time-update` class which is now used for selecting the HTML element which should bear the relative time display in https://github.com/Automattic/liveblog/blob/master/js/liveblog.js#L25

This commit adds the class to the appropriate `a` element and thus restores the functionality.

templates/liveblog-single-entry.php
@@ -2,7 +2,7 @@
 	<header class="liveblog-meta">
 		<span class="liveblog-author-avatar"><?php echo wp_kses_post( $avatar_img ); ?></span>
 		<span class="liveblog-author-name"><?php echo wp_kses_post( $author_link ); ?></span>
-		<span class="liveblog-meta-time"><a href="#liveblog-entry-<?php echo absint( $entry_id ); ?>"><span class="date"><?php echo esc_html( $entry_date ); ?></span><span class="time"><?php echo esc_html( $entry_time ); ?></span></a></span>
+		<span class="liveblog-meta-time"><a href="#liveblog-entry-<?php echo absint( $entry_id ); ?>" class="liveblog-time-update"><span class="date"><?php echo esc_html( $entry_date ); ?></span><span class="time"><?php echo esc_html( $entry_time ); ?></span></a></span>
 	</header>
 	<div class="liveblog-entry-text" data-original-content="<?php echo esc_attr( $original_content ); ?>">
 		<?php echo wp_kses( $content, $allowed_tags_for_entry ); ?>