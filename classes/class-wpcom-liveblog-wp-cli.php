0bd52d497cfa2892d77616f73f6a3176ed059c00
Olly Warren <olly@bigbitecreative.com>
2017-09-06 09:50:16 +0100

    Updated the WP_CLI command to fix the edited entries to include more output and a --dryrun flag that reports on changes expected to be made but doesnt persist them.

--- classes/class-wpcom-liveblog-wp-cli.php
+++ classes/class-wpcom-liveblog-wp-cli.php
@@ -13,35 +13,58 @@ class WPCOM_Liveblog_WP_CLI extends WP_CLI_Command {
 		$readme = $this->markdownify_headings( $readme );
 		echo $readme;
 	}
-	
+
 	/**
 	 * Fix wp_commentmeta table so archived liveblog posts comments display properly.
 	 *
 	 * @subcommand fix-archive
 	*/
-	public function fix_archive() {
+	public function fix_archive( $args, $assoc_args ) {
 		global $wpdb;
 
+		// Grab the dryrun flag from the assoc arguments if its there and define our falg as rwquired.
+		$is_dryrun = ( isset( $assoc_args['dryrun'] ) ) ? true : false;
+
 		// find all liveblogs
+		WP_CLI::line( 'Finding All Live Blog Entries..' );
+
 		$posts = new WP_Query( array(
 									'order'    => 'ASC',
 									'orderby'  => 'ID',
-									'meta_key' => 'liveblog' 
+									'meta_key' => 'liveblog'
 									) );
 
-		foreach( $posts->posts as $post ) {
+		//How many live blogs do we have?
+		$total_liveblogs = count($posts->posts);
+		$current_liveblog = 0;
+
+		//Feedback to the user
+		WP_CLI::line( 'Found ' . $total_liveblogs . ' Live Blogs.' );
+
+		foreach ($posts->posts as $post) {
+
+			//Increment the count so we get a more human readable index, inital value becomes 1 rather than 0.
+			$current_liveblog ++;
+
+			//Tell the user what we are doing, but lets colour this one se we can see its a new Liveblog in the console output.
+			// Note: Assigned to a varibale to be echo'd out, calling WP_CLI::colorize directly didnt output.
+			$intro = WP_CLI::colorize( "%4 Processing Liveblog {$current_liveblog} of {$total_liveblogs} %n");
+			echo $intro . PHP_EOL;
+
+		    //Define the post ID
 			$post_id = $post->ID;
 
 			// get all entries that have been edited in the liveblog
 			$entries_query = new WPCOM_Liveblog_Entry_Query( $post_id, WPCOM_Liveblog::key );
-			$edit_entries = $entries_query->get_all_edits( array( 'post_id' => $post_id ) );			
+			$edit_entries = $entries_query->get_all_edits( array( 'post_id' => $post_id ) );
+
 			// find correct comment_ids to replace incorrect meta_values
 			$correct_ids_array = $wpdb->get_results(
 				$wpdb->prepare(
 				"SELECT comment_id FROM $wpdb->comments
-				 WHERE comment_post_id = %d AND comment_id NOT IN 
+				 WHERE comment_post_id = %d AND comment_id NOT IN
 				 ( SELECT $wpdb->commentmeta.comment_id FROM $wpdb->commentmeta
-				   INNER JOIN $wpdb->comments 
+				   INNER JOIN $wpdb->comments
 				   ON $wpdb->comments.comment_id = $wpdb->commentmeta.comment_id
 				   WHERE comment_post_id = %d )
 				 ORDER BY comment_id ASC", $post_id, $post_id )
@@ -49,31 +72,37 @@ class WPCOM_Liveblog_WP_CLI extends WP_CLI_Command {
 			$correct_ids = wp_list_pluck( $correct_ids_array, 'comment_id' );
 
 			// replace incorrect meta_value with correct one
-			foreach( $edit_entries as $edit_entry ) {
-				$entry_id = $edit_entry->get_id();
+			if( count( $edit_entries ) > 0 ){
+				foreach( $edit_entries as $edit_entry ) {
+					$entry_id = $edit_entry->get_id();
 
-				// look for replaces property in $correct_ids 
-				if ( in_array( $edit_entry->replaces, $correct_ids ) ) {
-					continue;
-				}
-				else {
-					for ( $i = 0; $i <= count( $correct_ids ) - 1; $i++ )
-					{		
-						// replace with correct meta_value
-						if ( $correct_ids[$i] < $entry_id ) {
+					// look for replaces property in $correct_ids
+					if ( in_array( $edit_entry->replaces, $correct_ids ) ) {
 
-							$wpdb->update(
-								$wpdb->commentmeta,
-								array( 'meta_value' => $correct_ids[$i] ),
-								array( 'comment_id' => $entry_id )
-								);
+						continue;
+
+					} elseif( false == $is_dryrun ) {
+						// If this isnt a dry run we can run the database Update.
+						for ( $i = 0; $i <= count( $correct_ids ) - 1; $i++ ) {
+
+							// replace with correct meta_value
+							if ( $correct_ids[$i] < $entry_id ) {
+
+								$wpdb->update(
+									$wpdb->commentmeta,
+									array( 'meta_value' => $correct_ids[$i] ),
+									array( 'comment_id' => $entry_id )
+									);
+							}
 						}
 					}
 				}
 			}
+
+
 			// find comment_ids object with correct content for replacement
 			$correct_contents = $wpdb->get_results(
-				$wpdb->prepare( 
+				$wpdb->prepare(
 					"SELECT comment_id, comment_content
 					 FROM $wpdb->comments
 					 WHERE comment_post_id = %d
@@ -85,16 +114,16 @@ class WPCOM_Liveblog_WP_CLI extends WP_CLI_Command {
 			// find comment_ids that NEED to be replaced
 			$entries_replace = $wpdb->get_results(
 				$wpdb->prepare(
-					"SELECT DISTINCT meta_value 
-					FROM $wpdb->commentmeta 
-					INNER JOIN $wpdb->comments 
+					"SELECT DISTINCT meta_value
+					FROM $wpdb->commentmeta
+					INNER JOIN $wpdb->comments
 					ON $wpdb->comments.comment_id = $wpdb->commentmeta.comment_id
 					WHERE comment_post_id = %d
 					ORDER BY meta_value ASC", $post_id )
 				);
 
-			// check to make sure entry content being replaced matches available
-			if ( count( $entries_replace ) === count( $correct_contents) ) {
+			// check to make sure entry content being replaced matches available and that this is not a dry run
+			if ( count( $entries_replace ) === count( $correct_contents) && false === $is_dryrun ) {
 				// counter
 				$replaced = 0;
 				foreach( $entries_replace as $entry_replace ) {
@@ -106,11 +135,24 @@ class WPCOM_Liveblog_WP_CLI extends WP_CLI_Command {
 					 	array( 'comment_id' => $entry_replace->meta_value )
 					 	);
 
+					//Lets update the user with what we are doing.
+					WP_CLI::line( 'Replaced Content in ' . $replaced . ' Entry(ies) so far..' );
+
 					$replaced++;
 				}
 			}
+
+			//If we have a dry run flag lets just output what we would be looking to do on a live run.
+			if( true == $is_dryrun ) {
+				WP_CLI::line( 'Found ' . count($edit_entries) . ' Edited Entries on Post ID ' . $post_id . ' with ' . count( $entries_replace ) . ' Original Entries that need replacing with the correct contents.' );
+			}
 		}
-		WP_CLI::line( 'Fixed all entries on all liveblog posts!' );
+
+		if ( true == $is_dryrun ) {
+			WP_CLI::success( 'Dry Run Completed. Please check the results and when ready re-run the command without the --dryrun flag.' );
+		} else {
+			WP_CLI::success( 'Fixed all entries on all liveblog posts!' );
+		}
 	}
 
 	private function markdownify_headings( $readme ) {
