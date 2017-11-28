d5ccabaa4b78f55a686e01f34e2d690041df754e
Liam Defty <hello@liamdefty.com>
2017-11-28 17:22:36 +0000

    Begin writing unit and e2e tests

---
+++ src/react/__tests__/ete.test.js
@@ -0,0 +1,92 @@
+import puppeteer from 'puppeteer';
+
+const APP_ADMIN = 'http://10.0.10.27:3000/wp-admin';
+const APP_ADD_NEW = 'http://10.0.10.27:3000/wp-admin/post-new.php';
+const USER = 'bb_admin';
+const PASSWORD = 'admin';
+const TIMEOUT = 30000;
+
+let browser;
+let page;
+
+const renderEntry = async () => {
+  await page.click('.public-DraftEditor-content');
+  await page.keyboard.type('This is some test content');
+  await page.click('.liveblog-publish-btn');
+};
+
+const renderEntries = async (amount) => {
+  let times = amount;
+  if (times === 0) return Promise.resolve([]);
+  times -= 1;
+  const entries = await renderEntry().then(() => renderEntries(times));
+  return Promise.resolve(entries);
+};
+
+describe('End to End', async () => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ headless: false });
+    page = await browser.newPage();
+  }, TIMEOUT);
+
+  it('should create a new liveblog', async () => {
+    await page.goto(APP_ADMIN);
+    await page.evaluate((login, pass) => {
+      document.querySelector('#user_login').value = login;
+      document.querySelector('#user_pass').value = pass;
+      document.querySelector('#wp-submit').click();
+    }, USER, PASSWORD);
+
+    await page.waitForNavigation();
+    await page.goto(APP_ADD_NEW, { waitUntil: 'load' });
+    await page.click('input[name=post_title]');
+    await page.keyboard.type('Liveblog Test');
+    await page.click('#liveblog button[value=enable]');
+    await page.click('#publish');
+    await page.waitForNavigation();
+    await page.click('#message a');
+  }, TIMEOUT);
+
+  it('should render the editor', async () => {
+    await page.waitForSelector('.liveblog-editor-container');
+    const editor = await page.$('.liveblog-editor-container');
+    expect(editor).toBeDefined();
+  }, TIMEOUT);
+
+  it('should render an entry', async () => {
+    await page.waitForSelector('.public-DraftEditor-content');
+    await renderEntry();
+    await page.waitForSelector('.liveblog-entry');
+    const entry = await page.$('.liveblog-entry');
+    expect(entry).toBeDefined();
+  }, TIMEOUT);
+
+  it('should delete an entry', async () => {
+    await page.click('.liveblog-btn-delete');
+    const entryExists = await page.$$eval('.liveblog-entry', deleted => deleted);
+    expect(entryExists).toBeFalsy();
+  }, TIMEOUT);
+
+  it('should render multiple entries', async () => {
+    await renderEntries(15);
+    await page.reload({ waitUntil: ['load', 'networkidle0'] });
+    const pagination = await page.evaluate(() =>
+      document.querySelector('.liveblog-pagination-pages').innerHTML,
+    );
+    expect(pagination).toEqual('1 of 3');
+  }, TIMEOUT);
+
+  it('should render different pages', async () => {
+    await page.click('.liveblog-pagination-next');
+    const feedChildrenCount = await page.evaluate(() =>
+      document.querySelector('.liveblog-feed').children.length,
+    );
+    expect(feedChildrenCount).toEqual(5);
+  }, TIMEOUT);
+
+  afterAll(async () => {
+    await page.close();
+    await browser.disconnect();
+    await browser.close();
+  }, TIMEOUT);
+}, TIMEOUT);
---
+++ src/react/mockData/reducers/polling.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export default {
+  "entries": [
+      {
+          "id": "3250",
+          "type": "new",
+          "render": "<p>test</p>\n",
+          "content": "<p>test</p>",
+          "css_classes": "liveblog byuser comment-author-bb_admin bypostauthor even thread-even depth-1 liveblog-entry liveblog-entry-class-3250",
+          "timestamp": 1511882674,
+          "avatar_img": "<img alt='' src='http://1.gravatar.com/avatar/72465b47ce5554b3a30bdbbb21f5afeb?s=30&#038;d=mm&#038;r=g' srcset='http://1.gravatar.com/avatar/72465b47ce5554b3a30bdbbb21f5afeb?s=60&amp;d=mm&amp;r=g 2x' class='avatar avatar-30 photo' height='30' width='30' />",
+          "author_link": "bb_admin",
+          "entry_time": 1511882674,
+          "key_event": false
+      }
+  ],
+  "latest_timestamp": 1511882674,
+  "refresh_interval": 10,
+  "pages": 4
+}
