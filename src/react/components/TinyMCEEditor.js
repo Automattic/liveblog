fc951967bd8115b9601bd9fe0b1246b593715262
Adam Silverstein <adam@10up.com>
2018-05-02 18:41:03 -0400

    tinymce editor- sync content on load/change

--- src/react/components/TinyMCEEditor.js
+++ src/react/components/TinyMCEEditor.js
@@ -1,4 +1,4 @@
-/* global wp, jQuery, tinymce */
+/* global wp,  tinymce, jQuery, _ */
 /* eslint-disable no-return-assign */
 /* eslint-disable react/prop-types */
 
@@ -14,30 +14,29 @@ class TinyMCEEditor extends Component {
         plugins: 'charmap colorpicker fullscreen hr image  lists media paste tabfocus  wordpress wpautoresize wpdialogs wpeditimage wpgallery wplink wptextpattern wpview espnFootnote espnPromo espnPullquoteRight espnTwitter espnPullquote espnOrnamentalRule espnESPNVideo espnPodcasts espnDropcap',
         toolbar1: 'formatselect bold strikethrough bullist numlist blockquote alignleft aligncenter alignright link wp_more  wp_adv footnote ornamental-rule pullquote pullquote-right espn-video dropcap twitter promo podcasts | fullscreen',
         toolbar2: 'strikethru hr underline justifyfull forecolor | pastetext pasteword removeformat | media charmap | outdent indent | undo redo wp_help',
-
+        height: 300,
       },
       quicktags: true,
       mediaButtons: true,
     };
-  }
-  componentDidMount() {
-    jQuery(document).on('ready', () => {
-      this.editor = wp.editor.initialize(this.containerId, this.editorSettings);
-      jQuery(document).on('tinymce-editor-init', () => {
-        tinymce.editors.forEach((ed) => {
-          if (this.containerId === ed.id) {
-            ed.on('change blur', () => {
-              const content = ed.getContent();
-              if (content) {
-                this.props.editorContainer.setState({ rawText: content }, () => {
-                  this.props.editorContainer.syncRawTextToEditorState();
-                });
-              }
+    setTimeout(() => {
+      setTimeout(() => {
+        const currentEditor = _.findWhere(tinymce.editors, { id: this.containerId });
+        currentEditor.on('change', () => {
+          const content = currentEditor.getContent();
+          if (content) {
+            this.props.editorContainer.setState({ rawText: content }, () => {
+              this.props.editorContainer.syncRawTextToEditorState();
             });
           }
         });
-      });
-    });
+        const stateContent = this.props.editorContainer.getContent();
+        if (stateContent) {
+          tinymce.activeEditor.setContent(stateContent);
+        }
+      }, 2000);
+      wp.editor.initialize(this.containerId, this.editorSettings);
+    }, 2000);
   }
 
   render() {
