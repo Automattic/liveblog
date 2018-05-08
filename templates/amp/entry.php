cc1407f1ec8ef51f84c151ba6b34f425bf44ceb2
Scott Blackburn <scott@bigbitecreative.com>
2018-05-08 17:59:24 +0100

    Adds time offset

--- templates/amp/entry.php
+++ templates/amp/entry.php
@@ -2,9 +2,22 @@
   data-sort-time="20180424142720">
 
 	<aside class="liveblog-entry-aside">
+		<?php
+			// $offset = parseInt(utcOffset, 10);
+
+			// formattedTime(entry.entry_time, config.utc_offset, config.date_format)
+
+			// $entry_now = moment.unix(timestamp).utcOffset(offset, false).formatUsingDateTime(timeFormat);
+
+			$entry_time  =  $this->get('time');
+			$utc_offset  =  get_option( 'gmt_offset' ) . 'hours';
+			$date_format =  get_option( 'date_format' );
+			$entry_date =	date_i18n( $date_format, strtotime( $utc_offset,  $this->get('time') ) );
+			?>
+
 		<a class="liveblog-meta-time" href="#" target="_blank">
-			<span>1 day ago</span>
-			<span><?php echo esc_html ( $this->get('time') ); ?> </span>
+			<span><?php echo human_time_diff( $this->get('time'), current_time( 'timestamp', true ) ) . ' ago'  //echo esc_html ( $this->get('time') ); ?></span>
+			<span><?php echo esc_html( $entry_date ); ?> </span>
 		</a>
 	</aside>
 
