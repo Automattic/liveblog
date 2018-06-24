004d06166c25892d0a60937d905745abeeb81a11
Cain Hall <cain.hall98@gmail.com>
2018-06-25 09:37:37 +1000

    [WIP] Cleanup build

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const paths = {
   entry: './src/react/index.js',
@@ -15,6 +16,7 @@ const webpackConfig = {
   entry: {
     app: path.join(__dirname, paths.entry),
     amp: path.join(__dirname, './src/react/amp.js'),
+    vendor: ['react', 'moment'],
   },
 
   output: {
@@ -99,6 +101,14 @@ const webpackConfig = {
       __PROD__: JSON.stringify(process.env.NODE_ENV === 'production'),
       __TEST__: JSON.stringify(process.env.NODE_ENV === 'test'),
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env': {
+        NODE_ENV: 'production',
+      },
+    }),
+    new BundleAnalyzerPlugin(),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
     }),
   ],
 };
