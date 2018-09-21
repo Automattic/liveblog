29fb034189a825def8febdeb0daf7518cb357aaf
David Sword <david@davidsword.ca>
2018-09-21 08:30:45 -0700

    Add isPublishing state to Entries to reference update progress Re: #476
    When a user on a slower network/server clicked Publish Update, there was 
    no visual feedback that their action was processing.
    
    Adding this state to the entries by using pre-existing Redux actions, we 
    can know when the Publishing transaction is occurring, allowing 
    conditional checks within the components. This gives the option to 
    change the buttons text and classes, which can indicate progress.

--- src/react/reducers/user.js
+++ src/react/reducers/user.js
@@ -7,13 +7,50 @@ export const user = (state = initialState, action) => {
     case 'ENTRY_EDIT_OPEN':
       return {
         ...state,
-        entries: { ...state.entries, [action.payload]: { isEditing: true } },
+        entries: {
+          ...state.entries,
+          [action.payload]: {
+            isEditing: true,
+            isPublishing: false,
+          },
+        },
       };
 
     case 'ENTRY_EDIT_CLOSE':
       return {
         ...state,
-        entries: { ...state.entries, [action.payload]: { isEditing: false } },
+        entries: {
+          ...state.entries,
+          [action.payload]:
+          {
+            isEditing: false,
+            isPublishing: false,
+          },
+        },
+      };
+
+    case 'UPDATE_ENTRY':
+      return {
+        ...state,
+        entries: {
+          ...state.entries,
+          [action.payload.id]: {
+            isEditing: true,
+            isPublishing: true,
+          },
+        },
+      };
+
+    case 'UPDATE_ENTRY_SUCCESS':
+      return {
+        ...state,
+        entries: {
+          ...state.entries,
+          [action.payload.entries[0].id]: {
+            isEditing: false,
+            isPublishing: false,
+          },
+        },
       };
 
     default:
