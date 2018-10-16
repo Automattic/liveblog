333fa608a577abb22379ed22e794e5faa7d3eb94
Paul Schreiber <paul.schreiber@espn.com>
2018-10-16 12:21:09 -0400

    suppress warning for A8C_PROXIED_REQUEST

--- wpcom-helper.php
+++ wpcom-helper.php
@@ -149,7 +149,7 @@ add_filter(
 	function( $can_edit ) {
 
 		// Retain super admin access for A12s.
-		if ( is_automattician() || ( defined( 'A8C_PROXIED_REQUEST' ) && A8C_PROXIED_REQUEST ) ) {
+		if ( is_automattician() || ( defined( 'A8C_PROXIED_REQUEST' ) && A8C_PROXIED_REQUEST ) ) { // phpcs:ignore WordPressVIPMinimum.Constants.ConstantRestrictions.ConstantRestrictions
 			return $can_edit;
 		}
 
