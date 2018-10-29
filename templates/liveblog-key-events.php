613d53432c826cdf834c73a192f79b3d78a5f20c
Paul Schreiber <paul.schreiber@espn.com>
2018-10-29 12:12:21 -0400

    PHPCS whitespace fix

--- templates/liveblog-key-events.php
+++ templates/liveblog-key-events.php
@@ -1,5 +1,6 @@
-<?php if( 'false' != $title ): ?>
-	<div id="liveblog-key-events" data-title="<?php echo esc_attr( $title ) ?>"></div>
-<?php else: ?>
+<?php if ( 'false' != $title ) : ?>
+	<div id="liveblog-key-events" data-title="<?php echo esc_attr( $title ); ?>"></div>
+<?php else : ?>
 	<div id="liveblog-key-events"></div>
-<?php endif;
+	<?php 
+endif;
