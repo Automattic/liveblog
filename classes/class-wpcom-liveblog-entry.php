8103b196840bd4ac794a63ff1b4d877701e3977f
Christian Chung <christian.m.chung@gmail.com>
2018-11-28 12:04:03 -0500

    Accept entry objects and instances of WPCOM_Liveblog_Entry

--- classes/class-wpcom-liveblog-entry.php
+++ classes/class-wpcom-liveblog-entry.php
@@ -542,11 +542,13 @@ class WPCOM_Liveblog_Entry {
 	 * @return string        Image src.
 	 */
 	public static function get_entry_first_image( $entry ) {
-		$key    = 'liveblog_entry_' . $entry->id . 'first_image';
+		$entry_id = ( $entry instanceof WPCOM_Liveblog_Entry ) ? $entry->get_id() : $entry->id;
+		$entry_content = ( $entry instanceof WPCOM_Liveblog_Entry ) ? $entry->get_content() : $entry->content;
+		$key    = 'liveblog_entry_' . $entry_id . 'first_image';
 		$cached = wp_cache_get( $key, 'liveblog' );
 		
 		if ( false === $cached ) {
-			preg_match( '/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $entry->content, $image );		
+			preg_match( '/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $entry_content, $image );		
 			$cached = isset( $image['src'] ) ? $image['src'] : '';	
 			wp_cache_set( $key, $cached, 'liveblog' );
 		}
