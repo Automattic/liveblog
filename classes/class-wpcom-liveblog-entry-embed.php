127fe66644305a70fb49c1733e410873fa0886bd
Paul Schreiber <paul.schreiber@espn.com>
2019-07-26 16:11:48 -0400

    update entry-embed class to use posts instead of comments

--- classes/class-wpcom-liveblog-entry-embed.php
+++ classes/class-wpcom-liveblog-entry-embed.php
@@ -3,7 +3,6 @@
 /**
  * Class WPCOM_Liveblog_Entry_Embed
  *
- * Store oembeds in comment meta instead of post meta.
  */
 class WPCOM_Liveblog_Entry_Embed extends WP_Embed {
 
@@ -16,7 +15,7 @@ class WPCOM_Liveblog_Entry_Embed extends WP_Embed {
 	 * an AJAX request that will call WP_Embed::cache_oembed().
 	 *
 	 * Override the default method in a way it does nothing as
-	 * we don't need to pre-populate the cache as comments are
+	 * we don't need to pre-populate the cache as posts are
 	 * being created on the fly on frontend
 	 */
 	public function maybe_run_ajax_cache() {
@@ -35,15 +34,15 @@ class WPCOM_Liveblog_Entry_Embed extends WP_Embed {
 	 *     @type int $height Height of the embed in pixels.
 	 * }
 	 * @param string $url The URL attempting to be embedded.
-	 * @param int $comment_id The Comment ID of currently processed comment
+	 * @param int $entry_id The post ID of currently processed post
 	 * @return string|false The embed HTML on success, otherwise the original URL.
 	 *                      `->maybe_make_link()` can return false on failure.
 	 */
-	public function shortcode( $attr, $url = '', $comment = null ) {
-		if ( ! empty( $comment ) ) {
-			$comment = get_comment( $comment );
+	public function shortcode( $attr, $url = '', $entry = null ) {
+		if ( ! empty( $entry ) ) {
+			$entry = get_post( $entry );
 		} else {
-			$comment = get_comment();
+			$entry = get_post();
 		}
 
 		if ( empty( $url ) && ! empty( $attr['src'] ) ) {
@@ -97,15 +96,15 @@ class WPCOM_Liveblog_Entry_Embed extends WP_Embed {
 			}
 		}
 
-		$comment_id = ( ! empty( $comment->comment_ID ) ) ? $comment->comment_ID : null;
-		if ( ! empty( $this->comment_ID ) ) { // Potentially set by WPCOM_Comments_Embed::autoembed()
-			$comment_id = $this->comment_ID;
+		$entry_id = ( ! empty( $entry->ID ) ) ? $entry->ID : null;
+		if ( ! empty( $this->ID ) ) { // Potentially set by WPCOM_Comments_Embed::autoembed()
+			$entry_id = $this->ID;
 		}
 
 		// Unknown URL format. Let oEmbed have a go.
-		if ( $comment_id ) {
+		if ( $entry_id ) {
 
-			// Check for a cached result (stored in the comment meta)
+			// Check for a cached result (stored in the post meta)
 			$key_suffix    = md5( $url . wp_json_encode( $attr ) );
 			$cachekey      = '_oembed_' . $key_suffix;
 			$cachekey_time = '_oembed_time_' . $key_suffix;
@@ -116,23 +115,23 @@ class WPCOM_Liveblog_Entry_Embed extends WP_Embed {
 			 * @param int    $time       Time to live (in seconds).
 			 * @param string $url        The attempted embed URL.
 			 * @param array  $attr       An array of shortcode attributes.
-			 * @param int    $comment_id Comment ID.
+			 * @param int    $entry_id   Post ID.
 			 */
-			$ttl = apply_filters( 'oembed_ttl', DAY_IN_SECONDS, $url, $attr, $comment_id );
+			$ttl = apply_filters( 'oembed_ttl', DAY_IN_SECONDS, $url, $attr, $entry_id );
 
-			$cache      = get_comment_meta( $comment_id, $cachekey, true );
-			$cache_time = get_comment_meta( $comment_id, $cachekey_time, true );
+			$cache      = get_post_meta( $entry_id, $cachekey, true );
+			$cache_time = get_post_meta( $entry_id, $cachekey_time, true );
 
 			/**
-			 * Check post meta in case there is no existing comment meta
+			 * Check post meta in case there is no existing post meta
 			 * Odds are that related post meta exists and we should use
 			 * that one in order to not make existing Liveblogs to explode
-			 * before we fully transition to comment meta caching
+			 * before we fully transition to post meta caching
 			 */
 			if ( true === empty( $cache_time ) && true === empty( $cache ) ) {
-				$comment = get_comment( $comment_id );
-				if ( true === is_a( $comment, 'WP_Comment' ) ) {
-					$post_id    = $comment->comment_post_ID;
+				$entry = get_post( $entry_id );
+				if ( true === is_a( $entry, 'WP_Post' ) ) {
+					$post_id    = $entry->post_parent;
 					$cache      = get_post_meta( $post_id, $cachekey, true );
 					$cache_time = get_post_meta( $post_id, $cachekey_time, true );
 				}
@@ -156,12 +155,12 @@ class WPCOM_Liveblog_Entry_Embed extends WP_Embed {
 					 *
 					 * @see WP_Embed::shortcode()
 					 *
-					 * @param mixed  $cache      The cached HTML result, stored in comment meta.
+					 * @param mixed  $cache      The cached HTML result, stored in post meta.
 					 * @param string $url        The attempted embed URL.
 					 * @param array  $attr       An array of shortcode attributes.
-					 * @param int    $comment_id Comment ID.
+					 * @param int    $entry_id   Post ID.
 					 */
-					return apply_filters( 'embed_oembed_html', $cache, $url, $attr, $comment_id );
+					return apply_filters( 'embed_oembed_html', $cache, $url, $attr, $entry_id );
 				}
 			}
 
@@ -179,10 +178,10 @@ class WPCOM_Liveblog_Entry_Embed extends WP_Embed {
 
 			// Maybe cache the result
 			if ( $html ) {
-				update_comment_meta( $comment_id, $cachekey, $html );
-				update_comment_meta( $comment_id, $cachekey_time, time() );
+				update_post_meta( $entry_id, $cachekey, $html );
+				update_post_meta( $entry_id, $cachekey_time, time() );
 			} elseif ( ! $cache ) {
-				update_comment_meta( $comment_id, $cachekey, '{{unknown}}' );
+				update_post_meta( $entry_id, $cachekey, '{{unknown}}' );
 			}
 
 			// If there was a result, return it
@@ -195,9 +194,9 @@ class WPCOM_Liveblog_Entry_Embed extends WP_Embed {
 				 * @param mixed  $cache      The cached HTML result, stored in post meta.
 				 * @param string $url        The attempted embed URL.
 				 * @param array  $attr       An array of shortcode attributes.
-				 * @param int    $comment_id Comment ID.
+				 * @param int    $entry_id   Comment ID.
 				 */
-				return apply_filters( 'embed_oembed_html', $html, $url, $attr, $comment_id );
+				return apply_filters( 'embed_oembed_html', $html, $url, $attr, $entry_id );
 			}
 		}
 
@@ -208,41 +207,41 @@ class WPCOM_Liveblog_Entry_Embed extends WP_Embed {
 	/**
 	 * Delete all Comment oEmbed caches.
 	 *
-	 * @param int $comment_id Comment ID to delete the caches for.
+	 * @param int $entry_id post ID to delete the caches for.
 	 */
-	public function delete_oembed_caches( $comment_id = 0 ) {
-		if ( ! $comment_id ) {
-			$comment_id = get_comment_ID();
+	public function delete_oembed_caches( $entry_id = 0 ) {
+		if ( ! $entry_id ) {
+			$entry_id = get_the_ID();
 		}
-		$comment_metas = get_comment_meta( $comment_id );
-		if ( empty( $comment_metas ) ) {
+		$post_metas = get_post_meta( $entry_id );
+		if ( empty( $post_metas ) ) {
 			return;
 		}
-		if ( ! is_array( $comment_metas ) ) {
+		if ( ! is_array( $post_metas ) ) {
 			return;
 		}
-		$comment_meta_keys = array_keys( $comment_metas );
-		if ( ! $comment_meta_keys ) {
+		$post_meta_keys = array_keys( $post_metas );
+		if ( ! $post_meta_keys ) {
 			return;
 		}
 
-		foreach ( $comment_meta_keys as $comment_meta_key ) {
-			if ( '_oembed_' === substr( $comment_meta_key, 0, 8 ) ) {
-				delete_comment_meta( $comment_id, $comment_meta_key );
+		foreach ( $post_meta_keys as $post_meta_key ) {
+			if ( '_oembed_' === substr( $post_meta_key, 0, 8 ) ) {
+				delete_post_meta( $entry_id, $post_meta_key );
 			}
 		}
 	}
 
 	/**
-	 * Triggers a caching of all comment's oEmbed results.
+	 * Triggers a caching of all post's oEmbed results.
 	 *
 	 * The function is not implemented and overrides the
 	 * parent class' one. We don't need to pre-populate cache
-	 * for comments are they are being created on the fly.
+	 * for posts are they are being created on the fly.
 	 *
-	 * @param int $comment_id Comment ID to do the caching for.
+	 * @param int $entry_id Post ID to do the caching for.
 	 */
-	public function cache_oembed( $comment_id ) {
+	public function cache_oembed( $entry_id ) {
 	}
 
 	/**
@@ -266,15 +265,15 @@ class WPCOM_Liveblog_Entry_Embed extends WP_Embed {
 	 * @uses WP_Embed::autoembed_callback()
 	 *
 	 * @param string $content The content to be searched.
-	 * @param int|WP_Comment $comment object or integer representing the Comment ID
+	 * @param int|WP_Post $emtry object or integer representing the Post ID
 	 * @return string Potentially modified $content.
 	 */
-	public function autoembed( $content, $comment = null ) {
-		// Save comment's ID for later use - needed for Liveblog plugin compatibility as it calls the autoembed method directly
-		if ( ! empty( $comment ) ) {
-			$comment = get_comment( $comment );
-			if ( is_a( $comment, 'WP_Comment' ) ) {
-				$this->comment_ID = $comment->comment_ID;
+	public function autoembed( $content, $entry = null ) {
+		// Save post's ID for later use - needed for Liveblog plugin compatibility as it calls the autoembed method directly
+		if ( ! empty( $entry ) ) {
+			$entry = get_post( $entry );
+			if ( is_a( $entry, 'WP_Post' ) ) {
+				$this->ID = $entry->ID;
 			}
 		}
 
