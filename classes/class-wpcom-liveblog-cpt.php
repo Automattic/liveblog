f469a2fd0481709f6c83f8c72b5a6dec04700c35
Ivan Lopez <ivanlopezvp@gmail.com>
2019-08-19 09:49:05 -0400

    run phpcs

--- classes/class-wpcom-liveblog-cpt.php
+++ classes/class-wpcom-liveblog-cpt.php
@@ -45,7 +45,7 @@ class WPCOM_Liveblog_CPT {
 	public static function delete_children( $parent ) {
 
 		// Remove the query filter.
-		remove_filter( 'parse_query', [  __CLASS__, 'hierarchical_posts_filter' ] );
+		remove_filter( 'parse_query', [ __CLASS__, 'hierarchical_posts_filter' ] );
 		remove_filter( 'pre_get_posts', [ __CLASS__, 'filter_children_from_query' ] );
 		$parent = (int) $parent; // Force a cast as an integer.
 
@@ -58,11 +58,11 @@ class WPCOM_Liveblog_CPT {
 
 		// Get all children
 		$children = get_children(
-			array(
+			[
 				'post_type'        => self::$cpt_slug,
 				'post_parent'      => $parent,
-				'suppress_filters' => true,
-			)
+				'suppress_filters' => false,
+			]
 		);
 
 		// Remove the action so it doesn't fire again
