718bb5f7097e2202d7dfa7f3c3e7dd975b47fc73
Paul Schreiber <paul.schreiber@espn.com>
2019-09-13 12:13:53 -0400

    remove google plus from social share options

--- classes/class-wpcom-liveblog-amp.php
+++ classes/class-wpcom-liveblog-amp.php
@@ -90,7 +90,7 @@ class WPCOM_Liveblog_AMP {
 	 * Add default social share options
 	 */
 	public static function add_social_share_options() {
-		$social_array = [ 'twitter', 'pinterest', 'email', 'gplus' ];
+		$social_array = [ 'twitter', 'pinterest', 'email' ];
 
 		/**
 		 * Filter Liveblog AMP Facebook share app id
