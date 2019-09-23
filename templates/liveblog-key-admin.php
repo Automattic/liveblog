381361c8c5dcaa316e5bfdb74ea69f7b9c3f8469
Paul Schreiber <paul.schreiber@espn.com>
2019-09-23 17:33:27 -0400

    space out admin meta UI

--- templates/liveblog-key-admin.php
+++ templates/liveblog-key-admin.php
@@ -13,7 +13,7 @@
 <hr/>
 <p>
 	<label for="liveblog-key-template-name"><?php echo esc_html( $key_name ); ?> </label>
-	<select id="liveblog-key-template-name" name="liveblog-key-template-name">
+	<br /><select id="liveblog-key-template-name" name="liveblog-key-template-name">
 		<?php foreach ( $templates as $template ) : ?>
 		<option
 			<?php
@@ -27,7 +27,7 @@ selected="selected" <?php endif; ?> value="<?php echo esc_attr( $template ); ?>"
 		<?php endforeach; ?>
 	</select>
 	<label for="liveblog-key-template-format"><?php echo esc_html( $key_format_name ); ?> </label>
-	<select id="liveblog-key-template-format" name="liveblog-key-template-format">
+	<br /><select id="liveblog-key-template-format" name="liveblog-key-template-format">
 		<?php foreach ( $formats as $format ) : ?>
 			<option
 			<?php
@@ -41,8 +41,6 @@ selected="selected" <?php endif; ?> value="<?php echo esc_attr( $format ); ?>">
 		<?php endforeach; ?>
 	</select>
 	<label for="liveblog-key-limit"><?php echo esc_html( $key_limit ); ?></label>
-	<input id="liveblog-key-limit" type="text" name="liveblog-key-limit" value="<?php echo esc_attr( $current_key_limit ); ?>" />
-	<button type="button" class="button button-primary liveblog-key-template-save" value="liveblog-key-template-save"><?php echo esc_html( $key_button ); ?></button>
+	<br /><input id="liveblog-key-limit" type="text" name="liveblog-key-limit" value="<?php echo esc_attr( $current_key_limit ); ?>" />
 </p>
 <p class="howto"><?php echo esc_html( $key_description ); ?></p>
-<p>
