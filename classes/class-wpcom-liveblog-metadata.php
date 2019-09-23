09c2649437e9bcfbc1c084be27a5f53826c17778
Paul Schreiber <paul.schreiber@espn.com>
2019-09-23 16:36:50 -0400

    use liveblog_action for metabox

--- classes/class-wpcom-liveblog-metadata.php
+++ classes/class-wpcom-liveblog-metadata.php
@@ -20,12 +20,17 @@ class WPCOM_Liveblog_Metadata {
 	const METADATA_FORMAT         = '_liveblog_key_entry_format';
 	const METADATA_LIMIT          = '_liveblog_key_entry_limit';
 
+	public static $text_format   = '<p><label for="%1$s">%3$s</label><input type="%2$s" id="%1$s" class="widefat" name="%1$s" value="%4$s"/></p>';
+
+
 	/**
 	 * Called by WPCOM_Liveblog::load(),
 	 */
 	public static function load() {
 		add_action( 'add_meta_boxes', [ __CLASS__, 'add_meta_box' ] );
 		add_action( 'save_post', [ __CLASS__, 'save_post' ], 10, 3 );
+		add_action( 'liveblog_metadata', [ __CLASS__, 'liveblog_event_metadata' ] );
+		add_action( 'liveblog_metadata', [ __CLASS__, 'liveblog_slack_metadata' ] );
 	}
 
 	/**
@@ -115,45 +120,53 @@ class WPCOM_Liveblog_Metadata {
 	}
 
 	/**
+	 * Print a text field for the metabox
+	 *
+	 * @param  string $field_id DOM ID for field
+	 * @param  string $field_type type of field
+	 * @param  string $label label for field
+	 * @param  string $value value for field
+	 */
+	public static function print_text_field( $field_id, $field_type, $label, $value ) {
+		echo wp_kses( sprintf( self::$text_format, esc_attr( $field_id ), esc_attr( $field_type ), esc_html( $label ), esc_attr( $value ) ), WPCOM_Liveblog_Helpers::$meta_box_allowed_tags );
+	}
+
+	/**
 	 * Liveblog metadata metabox render callback.
 	 *
 	 * @param  \WP_Post $post The post object.
 	 * @return void
 	 */
 	public static function liveblog_metadata_metabox( $post ) {
+		wp_nonce_field( self::METADATA_NONCE_FIELD . $post->ID, self::METADATA_NONCE );
+
+		do_action( 'liveblog_metadata', $post );
+	}
+
+	public static function liveblog_event_metadata( $post ) {
 		$meta     = get_post_meta( $post->ID, self::METADATA_KEY, true );
 		$start    = isset( $meta[ self::METADATA_START_TIME ] ) ? $meta[ self::METADATA_START_TIME ] : '';
 		$end      = isset( $meta[ self::METADATA_END_TIME ] ) ? $meta[ self::METADATA_END_TIME ] : '';
 		$url      = isset( $meta[ self::METADATA_EVENT_URL ] ) ? $meta[ self::METADATA_EVENT_URL ] : '';
 		$title    = isset( $meta[ self::METADATA_EVENT_TITLE ] ) ? $meta[ self::METADATA_EVENT_TITLE ] : '';
 		$location = isset( $meta[ self::METADATA_EVENT_LOCATION ] ) ? $meta[ self::METADATA_EVENT_LOCATION ] : '';
-		$slack    = isset( $meta[ self::METADATA_SLACK_CHANNEL ] ) ? $meta[ self::METADATA_SLACK_CHANNEL ] : '';
-		$limit    = isset( $meta[ self::METADATA_LIMIT ] ) ? $meta[ self::METADATA_LIMIT ] : '';
-
-		$format   = '<p><label for="%1$s">%3$s</label><input type="%2$s" id="%1$s" class="widefat" name="%1$s" value="%4$s"/></p>';
-		$name     = function( $key ) {
-			return WPCOM_Liveblog_Metadata::METADATA_KEY . '[' . $key . ']';
-		};
-
-		wp_nonce_field( self::METADATA_NONCE_FIELD . $post->ID, self::METADATA_NONCE );
-
-		do_action( 'liveblog_before_metadata', $post );
-
-		echo '<hr>';
-		echo wp_kses( sprintf( $format, esc_attr( $name( self::METADATA_LIMIT ) ), 'text', 'Limit', $limit ), WPCOM_Liveblog_Helpers::$meta_box_allowed_tags );
-
-		echo wp_kses( sprintf( $format, esc_attr( $name( self::METADATA_SLACK_CHANNEL ) ), 'text', 'Slack Channel ID', $slack ), WPCOM_Liveblog_Helpers::$meta_box_allowed_tags );
 
 		echo '<hr>';
 		echo '<p><b>Event Metadata</b></p>';
 
-		echo wp_kses( sprintf( $format, esc_attr( $name( self::METADATA_START_TIME ) ), 'date', 'Coverage Start Date', $start ), WPCOM_Liveblog_Helpers::$meta_box_allowed_tags );
-		echo wp_kses( sprintf( $format, esc_attr( $name( self::METADATA_END_TIME ) ), 'date', 'Coverage End Date', $end ), WPCOM_Liveblog_Helpers::$meta_box_allowed_tags );
-		echo wp_kses( sprintf( $format, esc_attr( $name( self::METADATA_EVENT_TITLE ) ), 'text', 'Event Title', $title ), WPCOM_Liveblog_Helpers::$meta_box_allowed_tags );
-		echo wp_kses( sprintf( $format, esc_attr( $name( self::METADATA_EVENT_URL ) ), 'text', 'Event URL', $url ), WPCOM_Liveblog_Helpers::$meta_box_allowed_tags );
-		echo wp_kses( sprintf( $format, esc_attr( $name( self::METADATA_EVENT_LOCATION ) ), 'text', 'Event Location', $location ), WPCOM_Liveblog_Helpers::$meta_box_allowed_tags );
+		self::print_text_field( self::METADATA_START_TIME, 'date', 'Coverage Start Date', $start );
+		self::print_text_field( self::METADATA_END_TIME, 'date', 'Coverage End Date', $end );
+		self::print_text_field( self::METADATA_EVENT_TITLE, 'text', 'Event Title', $title );
+		self::print_text_field( self::METADATA_EVENT_URL, 'text', 'Event URL', $url );
+		self::print_text_field( self::METADATA_EVENT_LOCATION, 'text', 'Event Location', $location );
+	}
 
-		do_action( 'liveblog_after_metadata', $post );
+	public static function liveblog_slack_metadata( $post ) {
+		$meta     = get_post_meta( $post->ID, self::METADATA_KEY, true );
+		$slack    = isset( $meta[ self::METADATA_SLACK_CHANNEL ] ) ? $meta[ self::METADATA_SLACK_CHANNEL ] : '';
+
+		echo '<hr>';
+		self::print_text_field( self::METADATA_SLACK_CHANNEL, 'text', 'Slack Channel ID', $slack );
 	}
 
 	/**
