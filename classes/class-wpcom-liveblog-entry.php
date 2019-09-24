11c1f6a7f42d1d26772cdb054f1db3f56b999a6a
Paul Schreiber <paul.schreiber@espn.com>
2019-09-24 14:52:31 -0400

    allow 30-second cache times

--- classes/class-wpcom-liveblog-entry.php
+++ classes/class-wpcom-liveblog-entry.php
@@ -534,7 +534,7 @@ class WPCOM_Liveblog_Entry {
 			unset( $hidden_entries[ $post_id ] );
 		}
 
-		wp_cache_set( $cached_key, array_filter( $hidden_entries ), 'liveblog', 30 );
+		wp_cache_set( $cached_key, array_filter( $hidden_entries ), 'liveblog', 30 ); // phpcs:ignore WordPressVIPMinimum.Performance.LowExpiryCacheTime.LowCacheTime
 	}
 
 	/**
@@ -599,7 +599,7 @@ class WPCOM_Liveblog_Entry {
 		$entry->type                        = 'update';
 		$updated_entries[ $entry_post->ID ] = $entry->for_json();
 
-		wp_cache_set( $cached_key, $updated_entries, 'liveblog', 30 );
+		wp_cache_set( $cached_key, $updated_entries, 'liveblog', 30 ); // phpcs:ignore WordPressVIPMinimum.Performance.LowExpiryCacheTime.LowCacheTime
 	}
 
 	/**
