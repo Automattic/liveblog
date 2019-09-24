b71709ffdf93b8f67932cba28db211cf08b82886
Paul Schreiber <paul.schreiber@espn.com>
2019-09-24 13:22:45 -0400

    liveblog metabox: call save_liveblog_metadata action

--- classes/class-wpcom-liveblog-metadata.php
+++ classes/class-wpcom-liveblog-metadata.php
@@ -85,6 +85,8 @@ class WPCOM_Liveblog_Metadata {
 		$new_state = filter_input( INPUT_POST, 'state', FILTER_SANITIZE_STRING );
 		WPCOM_Liveblog::set_liveblog_state( $post_id, $new_state );
 
+		do_action( 'save_liveblog_metadata', $post );
+
 		// Save meta data.
 		$metadata = isset( $_POST[ self::METADATA_KEY ] ) ? wp_unslash( $_POST[ self::METADATA_KEY ] ) : false; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 
