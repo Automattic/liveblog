bcff7df7ab012b2656b1db0cbeac7addd404a069
Paul Schreiber <paul.schreiber@espn.com>
2019-10-03 10:38:47 -0400

    add ability to disable LB filtering dropdown

--- class-wpcom-liveblog.php
+++ class-wpcom-liveblog.php
@@ -62,11 +62,6 @@ if ( ! class_exists( 'WPCOM_Liveblog' ) ) :
 				return;
 			}
 			self::includes();
-			self::add_actions();
-			self::add_filters();
-			self::add_admin_actions();
-			self::add_admin_filters();
-			self::register_embed_handlers();
 
 			WPCOM_Liveblog_Entry_Key_Events::load();
 			WPCOM_Liveblog_Entry_Key_Events_Widget::load();
@@ -83,6 +78,14 @@ if ( ! class_exists( 'WPCOM_Liveblog' ) ) :
 
 			// Activate the WP CRON Hooks.
 			WPCOM_Liveblog_Cron::load();
+		}
+
+		public static function hooks() {
+			self::add_actions();
+			self::add_filters();
+			self::add_admin_actions();
+			self::add_admin_filters();
+			self::register_embed_handlers();
 		}
 
 		public static function add_custom_post_type_support( $query ) {
@@ -252,8 +255,10 @@ if ( ! class_exists( 'WPCOM_Liveblog' ) ) :
 				return;
 			}
 
-			add_action( 'restrict_manage_posts', [ __CLASS__, 'add_post_filtering_dropdown_to_manage_posts' ] );
-			add_action( 'pre_get_posts', [ __CLASS__, 'handle_query_vars_for_post_filtering' ] );
+			if ( apply_filters( 'liveblog_add_post_filtering_dropdown', true ) ) {
+				add_action( 'restrict_manage_posts', [ __CLASS__, 'add_post_filtering_dropdown_to_manage_posts' ] );
+				add_action( 'pre_get_posts', [ __CLASS__, 'handle_query_vars_for_post_filtering' ] );
+			}
 		}
 
 		/**
@@ -1993,7 +1998,9 @@ if ( ! class_exists( 'WPCOM_Liveblog' ) ) :
 		}
 
 	}
+
 	WPCOM_Liveblog::load();
+	add_action( 'after_setup_theme', [ 'WPCOM_Liveblog', 'hooks' ] );
 
 	/** Plupload Helpers ******************************************************/
 	if ( ! function_exists( 'wp_convert_hr_to_bytes' ) ) {
